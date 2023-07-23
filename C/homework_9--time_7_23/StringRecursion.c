28e5c5a088f6f3dd2885e52955d58f9a9d08c21b
GitGitHubGiteeGitLab <130532493+GitGitHubGiteeGitLab@users.noreply.github.com>
2023-07-23 20:52:21 +0800

    recursion

---
+++ C/homework_9--time_7_23/StringRecursion.c
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+﻿#define MAX_N 50
+
+char* reverse_string(char* string) {
+    char ch = *string;
+    if (*(string + 1) == '\0')return string;//递归出口
+    //递归过程
+    char* str = reverse_string(string + 1);
+    //处理过程
+    while (*str) {
+        *(str - 1) = *str;
+        str++;
+    }
+    *(str - 1) = ch;
+    return string;//结果返回
+}
+
+int main() {
+    char string[MAX_N];
+    scanf("%s", string);
+    char* str = reverse_string(string);
+    printf("%s", str);
+    return 0;
+}
---
+++ C/homework_9--time_7_23/StrlenSimulation.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+﻿#define MAX_N 50
+
+size_t mstrlen1(char* str) {
+    if (!(*str))return 0;
+    //++str风险大，回来str变了
+    return 1 + mstrlen(str + 1);//str没变
+}
+
+int main() {
+    char str[MAX_N];
+    scanf("%s", str);
+    size_t len = mstrlen(str);
+    printf("%zd\n", len);
+    return 0;
+}
