ac198ed062104cb1247412882306f3d04b648783
GitGitHubGiteeGitLab <130532493+GitGitHubGiteeGitLab@users.noreply.github.com>
2023-07-23 23:18:47 +0800

    hanota老六做法

---
+++ C/homework_9--time_7_23/hanota_1.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hanota_1.c
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 23 Jul 2023 11:16:27 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+//老六做法
+void hanota(int* A,  int ASize, int* B, int BSize, int** C, int* CSize){ 
+  *CSize=ASize;
+    *C=A;
+}
